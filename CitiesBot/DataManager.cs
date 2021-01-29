c2e4942e373bafd895fa33c72e688eb5b87a077b
kiwi-juicee <kiranixis@gmail.com>
2021-01-29 18:44:51 +0300

    списки городов, обращения к ним

---
+++ CitiesBot/DataManager.cs
@@ -0,0 +1,67 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CitiesBot
+{
+    public class DataManager
+    {
+        public List<string> Cities;
+        public List<string> BigCities;
+        private string citiespath = @"Txts\allcities.txt";
+        private string bigcitiespath = @"Txts\allcities.txt";
+        public DataManager()
+        {
+            Cities = new List<string>();
+            BigCities = new List<string>();
+            GetLists();
+        }
+        private void GetLists()
+        {
+            foreach (string city in File.ReadAllLines(citiespath))
+                Cities.Add(city);
+            foreach (string city in File.ReadAllLines(bigcitiespath))
+                BigCities.Add(city);
+        }
+        private string CityNameHandler(string city)
+        {
+            string name = "";
+            string[] words = city.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = ToUpper(words[i]);
+                if (words[i].Contains('-'))
+                {
+                    string[] arr = words[i].Split('-');
+                    foreach (var part in arr)
+                    {
+                        name += ToUpper(part) + "-";
+                    }
+                }
+                else
+                    name += words[i] + " ";
+            }
+            name = name.Remove(name.Length - 1, 1);
+            return name;
+        }
+        private string ToUpper(string str)
+        {
+            if (str != "")
+                str = str.Substring(0, 1).ToUpper() + str.Remove(0, 1);
+            return str;
+        }
+        public void AddCity(string city)
+        {
+            string name = CityNameHandler(city);
+            File.AppendAllText(citiespath, "\n" + name);
+        }
+        public bool IsCityExist(string name)
+        {
+            IEnumerable<string> list = File.ReadAllLines(citiespath);
+            return list.Contains(CityNameHandler(name.ToLower()));
+        }
+    }
+}
--- CitiesBot/Program.cs
+++ CitiesBot/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Telegram.Bot;
 
 namespace CitiesBot
 {
@@ -11,17 +10,18 @@ namespace CitiesBot
     {
         static private string key = "1574797346:AAH5zYFinhCmrKtrbUuX8b2YdvS0LR5bdmc";
         static private Telega bot;
+        static private DataManager manager = new DataManager();
 
         static void Main(string[] args)
         {
             Initialize();
             Console.WriteLine("Бот запущен");
-            Console.ReadLine();
         }
         static private void Initialize()
         {
             bot = new Telega(key);
             bot.GotMessage += Bot_GotMessage;
+            
         }
         static async private void Bot_GotMessage(Telega bot, string msg, long chatid)
         {
